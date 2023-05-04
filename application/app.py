5d77aa05c6c1e154c32f1c5deaceef2c648b8d68
yaswanth836 <yaswanth836@gmail.com>
2023-05-04 16:14:32 -0400

    created profile page

--- application/app.py
+++ application/app.py
@@ -135,6 +135,10 @@ def logout():
 def rewards():
     return render_template('rewards.html')
 
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
 
 if __name__ == '__main__':
   app.run(debug=True)
